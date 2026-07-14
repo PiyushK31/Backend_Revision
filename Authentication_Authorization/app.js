d55c88b6eebdfc96852181be114c0c6c20fe0b07
PiyushK31 <piyushkrr80580@gmail.com>
2026-07-14 23:42:18 +0530

    Add new fles

---
+++ Authentication_Authorization/app.js
@@ -0,0 +1,60 @@
+// Import required modules
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+// Import the User model
+const userModle = require('./models/user');
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: true })); // Parse form data
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+app.use(cookieParser()); // Parse cookies
+
+// Home route
+// Renders the registration page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Create User Route
+app.post('/Create', (req, res) => {
+
+    // Get user data from the form
+    let { username, email, password, age } = req.body;
+
+    // Generate a salt for password hashing
+    bcrypt.genSalt(10, (err, salt) => {
+
+        // Hash the user's password
+        bcrypt.hash(password, salt, async (err, hash) => {
+
+            // Save user details in MongoDB
+            let createdUser = await userModle.create({
+                username,
+                email,
+                password: hash, // Store the hashed password instead of the original
+                age
+            });
+
+            // Send the created user as a response
+            res.send(createdUser);
+        });
+    });
+});
+
+// Start the server
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err.message);
+    } else {
+        console.log("Server is running on port 3000");
+    }
+});
